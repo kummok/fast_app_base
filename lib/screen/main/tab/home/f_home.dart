b4c403130e9954d9dece49e54f035dd1d5317e23
Kyungmok Kum <kumsalang@hotmail.com>
2024-01-21 20:58:09 +0900

    08 home 만들기 완료

--- lib/screen/main/tab/home/f_home.dart
+++ lib/screen/main/tab/home/f_home.dart
@@ -2,7 +2,10 @@ import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/common/widget/round_button_theme.dart';
 import 'package:fast_app_base/common/widget/w_big_button.dart';
 import 'package:fast_app_base/common/widget/w_round_button.dart';
+import 'package:fast_app_base/common/widget/w_rounded_container.dart';
 import 'package:fast_app_base/screen/dialog/d_message.dart';
+import 'package:fast_app_base/screen/main/tab/home/bank_accounts_dummy.dart';
+import 'package:fast_app_base/screen/main/tab/home/w_bank_account.dart';
 import 'package:fast_app_base/screen/main/tab/home/w_ttoss_app_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -21,7 +24,7 @@ class HomeFragment extends StatelessWidget {
       child: Stack(
         children: [
           SingleChildScrollView(
-            padding: EdgeInsets.only(top:60),
+            padding: EdgeInsets.only(top: 60),
             child: Column(
               children: [
                 BigButton(
@@ -30,8 +33,17 @@ class HomeFragment extends StatelessWidget {
                     context.showSnackbar("토스뱅크를 눌렀어요.");
                   },
                 ),
+                height10,
+                RoundedContainer(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                  "자산".text.bold.white.make(),
+                  height5,
+                  ...bankAccounts.map((e)=> BankAccountWidget(e)).toList()
+                ],))
               ],
-            ).pSymmetric(h:20),
+            ).pSymmetric(h: 20),
           ),
           const TtossAppBar(),
         ],
